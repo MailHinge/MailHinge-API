a3660f3789b047023c490fe15a9c864698edf47e
legacy5813 <hafiirahman200@gmail.com>
2023-05-05 22:42:41 +0100

    gmail controller v1

---
+++ Controllers/GmailController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MailHinge_API.Interfaces;
+using Google.Apis.Gmail.v1;
+
+namespace MailHinge_API.Controllers
+{
+    [ApiController]
+    [Route("gmail/")]
+
+    public class GmailController : ControllerBase
+	{
+		private readonly IGMailService _IGMailService;
+
+		public GmailController(IGMailService gmailService)
+		{
+			_IGMailService = gmailService;
+		}
+
+        [HttpGet("getEmail")]
+		public string GetEmail()
+		{
+			var serviceResult = _IGMailService.GetGmailServiceClient();
+			
+
+			return "success";
+		}
+
+
+
+    }
+}
+
