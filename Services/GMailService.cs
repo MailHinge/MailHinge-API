09c9c01812e40fe35b28c8cd3f673d210231fd47
legacy5813 <hafiirahman200@gmail.com>
2023-05-07 20:12:53 +0100

    client update test

--- Services/GMailService.cs
+++ Services/GMailService.cs
@@ -16,7 +16,9 @@ namespace MailHinge_API.Services
         {
             // Create a new Gmail API client with your credentials
             var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                new ClientSecrets { ClientId = "1010069659275-05ria9gg4uei23tvdn89ebe5av0aht74.apps.googleusercontent.com", ClientSecret = "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" },
+                new ClientSecrets { ClientId = "1010069659275-2n1ovsplhcumjagac61vjer0lo35o1r7.apps.googleusercontent.com",
+                    ClientSecret = "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"
+                },
                 new[] { GmailService.Scope.GmailReadonly },
                 "user",
                 CancellationToken.None);
