633788114f6c0c393f8734ff6db01fd395a05e73
legacy5813 <hafiirahman200@gmail.com>
2023-05-10 21:57:16 +0100

    start congiurations for auth

--- Services/GMailService.cs
+++ Services/GMailService.cs
@@ -6,54 +6,19 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MailHinge_API.Services
 {
-    public class GMailService : IGMailService
+    public class GMailService 
     {
-        private static string clientID = "1010069659275-2n1ovsplhcumjagac61vjer0lo35o1r7.apps.googleusercontent.com";
-        private static string clientSecret = "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";
-        private static string redirectURI = "http://localhost:7144/authorize";
+
 
         public GMailService()
         {
         }
 
-        public void GetGmailServiceClient(IServiceCollection gmailservice)
-        {
-            // This configures Google.Apis.Auth.AspNetCore3 for use in this app.
-            gmailservice
-                .AddAuthentication(o =>
-                {
-                    // This forces challenge results to be handled by Google OpenID Handler, so there's no
-                    // need to add an AccountController that emits challenges for Login.
-                    o.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
-                    // This forces forbid results to be handled by Google OpenID Handler, which checks if
-                    // extra scopes are required and does automatic incremental auth.
-                    o.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
-                    // Default scheme that will handle everything else.
-                    // Once a user is authenticated, the OAuth2 token info is stored in cookies.
-                    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                })
-                .AddCookie()
-                .AddGoogleOpenIdConnect(options =>
-                {
-                    options.ClientId = clientID;
-                    options.ClientSecret = clientSecret;
-                });
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            
-            app.UseHttpsRedirection();
-            
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            
-}
-
-
 
     }
+
+            
 }
+
+ 
 
--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Google.Apis.Auth.AspNetCore3;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MailHinge_API
 {
-	public class Startup
+    public class Startup
 	{
-        private static string clientID = "1010069659275-2n1ovsplhcumjagac61vjer0lo35o1r7.apps.googleusercontent.com";
-        private static string clientSecret = "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";
-        private static string redirectURI = "http://localhost:7144/authorize";
+        private const string clientID = "1010069659275-2n1ovsplhcumjagac61vjer0lo35o1r7.apps.googleusercontent.com";
+        private const string clientSecret = "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";
+        private const string redirectURI = "http://localhost:7144/authorize";
 
         public Startup()
 		{
@@ -19,11 +21,37 @@ namespace MailHinge_API
         public void ConfigureServices(IServiceCollection services)
         {
             // Configure your services here
+            // This configures Google.Apis.Auth.AspNetCore3 for use in this app.
+            services
+                .AddAuthentication(o =>
+                {
+                    // This forces challenge results to be handled by Google OpenID Handler, so there's no
+                    // need to add an AccountController that emits challenges for Login.
+                    o.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+                    // This forces forbid results to be handled by Google OpenID Handler, which checks if
+                    // extra scopes are required and does automatic incremental auth.
+                    o.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+                    // Default scheme that will handle everything else.
+                    // Once a user is authenticated, the OAuth2 token info is stored in cookies.
+                    o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddGoogleOpenIdConnect(options =>
+                {
+                    options.ClientId = clientID;
+                    options.ClientSecret = clientSecret;
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // Configure your middleware pipeline here
+            app.UseHttpsRedirection();
+           
+                
+            app.UseAuthentication();
+            app.UseAuthorization();
+
         }
     }
 }
