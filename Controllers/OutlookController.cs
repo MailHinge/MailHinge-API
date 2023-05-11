ba7cd4f9d845c692c794942cdd6256f4fbb84d3a
legacy5813 <hafiirahman200@gmail.com>
2023-05-11 22:20:10 +0100

    users method

--- Controllers/OutlookController.cs
+++ Controllers/OutlookController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Graph;
 using Azure.Identity;
 using MailHinge_API.Interfaces;
+using System.Linq;
 
 
 namespace MailHinge_API.Controllers
@@ -24,13 +25,19 @@ namespace MailHinge_API.Controllers
         [HttpGet("getEmail")]
         public async Task<string> GetEmail()
         {
-            var result = _outlookService.GetGraphServiceClient();
+            var graphClient = _outlookService.GetGraphServiceClient();
 
             string userEmail = "hafiirahman@outlook.com";
 
             // Call the Graph API to find the user by email address
-            var user = await result.Me
-            .GetAsync();
+            //var user = await result.Me
+            //.GetAsync()
+
+            var users = await graphClient.Users[userEmail].MailFolders["inbox"]
+                .Messages.GetAsync(requestConfiguration =>
+                {
+                    requestConfiguration.QueryParameters.Select = new string[] { "sender", "subject" };
+                });
 
             return "success";
         }
