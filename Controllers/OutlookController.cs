ddf768b34f79592e55211d03e33cb50c579a6800
Wojtek Piekarczyk <piekarczykw@gmail.com>
2023-05-17 22:54:22 +0100

    Some changes

--- Controllers/OutlookController.cs
+++ Controllers/OutlookController.cs
@@ -1,42 +1,41 @@
-﻿using System;
+﻿using Azure.Identity;
+using MailHinge_API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Graph;
-using Azure.Identity;
-using MailHinge_API.Interfaces;
-
 
 namespace MailHinge_API.Controllers
 {
     [ApiController]
     [Route("outlook/")]
-
     public class OutlookController : ControllerBase
-	{
+    {
         private readonly IOutlookService _outlookService;
 
-
         public OutlookController(IOutlookService outlookService)
-		{
+        {
             _outlookService = outlookService;
 
-		}
+        }
 
         [HttpGet("getEmail")]
         public async Task<string> GetEmail()
         {
-            var result = _outlookService.GetGraphServiceClient();
+            //var graphServiceClient = _outlookService.GetGraphServiceClient();
 
-            string userEmail = "hafiirahman@outlook.com";
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+            var tenantId = "f8cdef31-a31e-4b4a-93e4-5f571e91255a";
+            var clientId = "4b496d0e-e0ad-4c65-a8ee-3f603b78635d";
+            var clientSecret = "k2x8Q~3KjmBP-CC~46nwh1zM9Mr25_PK508~6b7G";
+            var clientSecretCredential = new ClientSecretCredential(
+                            tenantId, clientId, clientSecret);
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
 
-            // Call the Graph API to find the user by email address
-            var user = await result.Me
-            .GetAsync();
+            var result = await graphClient.Me.Messages.GetAsync((requestConfiguration) =>
+            {
+                requestConfiguration.QueryParameters.Select = new string[] { "sender", "subject" };
+            });
 
             return "success";
         }
-
-
-        
-	}
+    }
 }
-
--- Services/OutlookService.cs
+++ Services/OutlookService.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using Azure.Identity;
+﻿using Azure.Identity;
 using MailHinge_API.Interfaces;
 using Microsoft.Graph;
 
 namespace MailHinge_API.Services
 {
-	public class OutlookService : IOutlookService
+    public class OutlookService : IOutlookService
     {
-		public OutlookService()
-		{
-		}
+        public OutlookService()
+        {
+        }
 
         public GraphServiceClient GetGraphServiceClient()
         {
@@ -19,25 +18,23 @@ namespace MailHinge_API.Services
             // app registration in Azure. An administrator must grant consent
             // to those permissions beforehand.
             #endregion
-        string[] scopes = new[] { "https://graph.microsoft.com/.default" };
+            string[] scopes = new[] { "https://graph.microsoft.com/.default" };
 
-        //client, client secret and tenant IDs from app registration
-        string clientID = "4b496d0e-e0ad-4c65-a8ee-3f603b78635d";
-        string tenantID = "f8cdef31-a31e-4b4a-93e4-5f571e91255a";
-        string clientSecretVal = "k2x8Q~3KjmBP-CC~46nwh1zM9Mr25_PK508~6b7G";
+            //client, client secret and tenant IDs from app registration
+            string clientID = "4b496d0e-e0ad-4c65-a8ee-3f603b78635d";
+            string tenantID = "f8cdef31-a31e-4b4a-93e4-5f571e91255a";
+            string clientSecretVal = "k2x8Q~3KjmBP-CC~46nwh1zM9Mr25_PK508~6b7G";
 
-        //using azure.identity
-        TokenCredentialOptions options = new TokenCredentialOptions
-        {
-            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
-        };
+            //using azure.identity
+            TokenCredentialOptions options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
 
-        ClientSecretCredential clientSecretCredential = new ClientSecretCredential(
-        tenantID, clientID, clientSecretVal, options);
+            ClientSecretCredential clientSecretCredential = new ClientSecretCredential(
+            tenantID, clientID, clientSecretVal, options);
 
-        GraphServiceClient _graphService = new GraphServiceClient(clientSecretCredential, scopes);
-
-            return _graphService;
+            return new GraphServiceClient(clientSecretCredential, scopes);
         }
     }
 }
