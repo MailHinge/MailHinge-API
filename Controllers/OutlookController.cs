b91bea1265eb827b0224fad88a00a6ae90c981d4
legacy5813 <hafiirahman200@gmail.com>
2023-05-20 11:15:44 +0100

    graph client

--- Controllers/OutlookController.cs
+++ Controllers/OutlookController.cs
@@ -21,17 +21,9 @@ namespace MailHinge_API.Controllers
         [HttpGet("getEmail")]
         public async Task<string> GetEmail()
         {
-            //var graphServiceClient = _outlookService.GetGraphServiceClient();
+            var graphServiceClient = _outlookService.GetGraphServiceClient();
 
-            var scopes = new[] { "https://graph.microsoft.com/.default" };
-            var tenantId = "f8cdef31-a31e-4b4a-93e4-5f571e91255a";
-            var clientId = "4b496d0e-e0ad-4c65-a8ee-3f603b78635d";
-            var clientSecret = "k2x8Q~3KjmBP-CC~46nwh1zM9Mr25_PK508~6b7G";
-            var clientSecretCredential = new ClientSecretCredential(
-                            tenantId, clientId, clientSecret);
-            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
-
-            var result = await graphClient.Me.Messages.GetAsync((requestConfiguration) =>
+            var result = await graphServiceClient.Me.Messages.GetAsync((requestConfiguration) =>
             {
                 requestConfiguration.QueryParameters.Select = new string[] { "sender", "subject" };
             });
